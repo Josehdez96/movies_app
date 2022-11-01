5bb78b159b48dbf4053fef89b5ccfa3f82cc2d5c
Jose D Hernandez <josedanielhdezit@gmail.com>
2022-11-01 18:12:24 -0500

    feat: movie carousel

--- lib/widgets/movie_carousel.dart
+++ lib/widgets/movie_carousel.dart
@@ -5,41 +5,67 @@ class MovieCarousel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 160,
-      width: double.infinity,
-      color: Colors.red,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text('Populares'),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: SizedBox(
+        height: 180,
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(12, 10, 0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const _CarouselTitle(),
+              const SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 20,
+                  itemBuilder: (_, index) => _Poster(),
+                ),
+              )
+            ],
           ),
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 20,
-              itemBuilder: (_, index) => _MoviePoster(),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
 }
 
-class _MoviePoster extends StatelessWidget {
-  const _MoviePoster({
+class _CarouselTitle extends StatelessWidget {
+  const _CarouselTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Populares',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    );
+  }
+}
+
+class _Poster extends StatelessWidget {
+  const _Poster({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 100,
-      color: Colors.blue,
-      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+      margin: const EdgeInsets.only(right: 7),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: const Image(
+          width: 100,
+          fit: BoxFit.fill,
+          image: NetworkImage('https://via.placeholder.com/400x500'),
+        ),
+      ),
     );
   }
 }
