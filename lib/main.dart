bd55d6b2302833958f86c42e4f0ea89fc9dc1852
Jose D Hernandez <josedanielhdezit@gmail.com>
2022-11-18 15:59:07 -0500

    build: http added to pubspect

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'providers/movies_provider.dart';
 import 'providers/navigation_provider.dart';
 import 'screens/body_screen.dart';
 
 // https://via.placeholder.com/700x350
 // - SliverAppBar con imagen random y sus iconos (iguala el comportamiento del del netflix)
 // - ENDPOINT PELICULAS: https://api.themoviedb.org/3/movie/now_playing?api_key=16d550d36c7c3c667bfc2f352f4fbda0&language=es-ES&page=1
+// - URL IMAGES: https://image.tmdb.org/t/p/w500/[kqjL17yufvn9OVLyXYpvtyrFfak.jpg]
 
 void main() => runApp(MyApp());
 
@@ -14,7 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: ((_) => NavigationProvider()))
+        ChangeNotifierProvider(create: ((_) => NavigationProvider())),
+        ChangeNotifierProvider(create: ((_) => MoviesProvider())),
       ],
       child: MaterialApp(
         title: 'Movies App',
